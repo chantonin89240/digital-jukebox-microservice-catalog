6996e9ba83707bd2c40b9c43f7c2ed2519a5a702
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2023-10-25 17:01:30 +0200

    fonction search Deezer

---
+++ Micro-service-catalog/Application/Catalogs/Queries/SearchDeezer/AlbumDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Catalogs.Queries.SearchDeezer;
+
+public class AlbumDto
+{
+    public long Id { get; set; }
+    public string? Title { get; set; }
+    public string? Cover { get; set; }
+    public string? Cover_small { get; set; }
+    public string? Cover_big { get; set; }
+    public string? Cover_xl { get; set; }
+    public string? Md5_image { get; set; }
+    public string? Tracklist { get; set; }
+    public string? Type { get; set; }
+}
---
+++ Micro-service-catalog/Application/Catalogs/Queries/SearchDeezer/DeezerDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Catalogs.Queries.SearchDeezer;
+
+public class DeezerDto
+{
+    public long Id { get; set; }
+    public bool Readable { get; set; }
+    public string? Title { get; set; }
+    public string? Title_short { get; set; }
+    public string? Title_version { get; set; }
+    public string? Link { get; set; }
+    public long Duration { get; set; }
+    public long Rank { get; set; }
+    public bool explicit_lyrics { get; set; }
+    public long Explicit_content_lyrics { get; set; }
+    public long Explicit_content_cover { get; set; }
+    public string? Preview { get; set; }
+    public ArtistDto? Artist { get; set; }
+    public AlbumDto? Album { get; set; }
+    public string Type { get; set; }
+}
+
